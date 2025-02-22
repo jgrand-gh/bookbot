d115e709132b14770f236f610d24ea185ceb668d
Josh Grand <8271819+jgrand-gh@users.noreply.github.com>
2025-02-22 00:47:51 -0600

    book.dev course updated, so making changes as needed

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+
 def main():
-    book_path = "books/frankenstein.txt"
+    print(sys.argv)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+    #book_path = "books/frankenstein.txt"
     contents = get_book_text(book_path)
 
     word_count = get_word_count(contents)
